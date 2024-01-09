ac6e504af02e44ec9e6247158e9ad48db9af6569
tungdoanduy <sion080803@gmail.com>
2024-01-09 10:42:59 +0700

    update some stuff

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,17 +7,51 @@ public class Player : MonoBehaviour
     [SerializeField] PhotonView photonView;
     [SerializeField] SO_Player role;
     [SerializeField] List<MeshRenderer> parts = new List<MeshRenderer>();
+    [SerializeField] Animator anim;
     Vector2Int currentTilePos;
     public Vector2Int CurrentTilePos
     {
         get => currentTilePos;
         set => currentTilePos = value;
     }
+
+    [SerializeField] GameObject solarShield;
+
     public void SetUp()
     {
         foreach (MeshRenderer part in parts)
         {
             part.material = role.mat;
         }
+    }
+
+    void DuneBlaster()
+    {
+
+    }
+
+    void Teleporter()
+    {
+
+    }
+
+    void BottleOfWater()
+    {
+
+    }
+
+    void SolarShield()
+    {
+
+    }
+
+    void Terrascope()
+    {
+
+    }
+
+    void Capsule()
+    {
+
     }
 }
